ae90e25ab745b9c1c1328cb33c1708b58f57db3c
AnushkaMohite2003 <86216718+anushkamohite2003@users.noreply.github.com>
2021-06-21 14:56:04 +0530

    Add files via upload

---
+++ Swapping of Two Numbers/01_Swapping_Of_two_Numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No1=0,No2=0;
+
+    printf("\n Enter Two Numbers=");
+    scanf("%d%d",&No1,&No2);
+
+    printf("\n Values of Given Number Before Swap ");
+    printf("\n No1=%d,No2=%d,No1,No2");
+
+
+    getch();
+    return 0;
+}
