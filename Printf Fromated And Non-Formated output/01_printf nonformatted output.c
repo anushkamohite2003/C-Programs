a00656da7b558250c42d56c1c57262e39251c61f
AnushkaMohite2003 <86216718+anushkamohite2003@users.noreply.github.com>
2021-06-21 14:52:05 +0530

    Add files via upload

---
+++ Printf Fromated And Non-Formated output/01_printf nonformatted output.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n %d %s %d %d %d %d %f",255,'shital ghare',200,300,200,100 ,100.00);
+    printf("\n %d %s %d %d %d %d %f",233,'amit pol',600,300,600,300,200,66.6666);
+    printf("\n %d %s %d %d %d %d %f",455,'dipti mane',500,300,200,500,300,70.00);
+
+    getch();
+    return 0;
+
+
+
+}
