2ce43b5f4b303eaa2d96bb1df9490e0ccfb7291e
AnushkaMohite2003 <86216718+anushkamohite2003@users.noreply.github.com>
2021-06-21 14:46:50 +0530

    Add files via upload

---
+++ Addition of Two Numbers/01_Addition off Two Numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int sum=0;
+void main()
+{
+    int Num1=0,Num2=0;
+    printf("\n Enter Two Numbers");
+    scanf("%d%d",&Num1,&Num2);
+    sum=Num1+Num2;
+    printf("\n Addition of %d & %d is = %d ,Num1,Num2,Num3");
+    getch()
+}
